9ee768044bdc532c5c6ea59feb05710fe93cf235
Alex R <alex@ironrobin.net>
2025-08-12 12:19:09 -0500

    add ci

---
+++ entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+cd /build
+
+
+repo_full=$(cat ./repo)
+repo_owner=$(echo $repo_full | cut -d/ -f1)
+repo_name=$(echo $repo_full | cut -d/ -f2)
+sed -i '/\[community\]/d' /etc/pacman.conf
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
+pacman-key --init
+pacman -Syu --noconfirm --needed sudo git wget python
+useradd builduser -m
+chown -R builduser:builduser /build
+git config --global --add safe.directory /build
+sudo -u builduser gpg --recv-keys 38DBBDC86092693E
+passwd -d builduser
+printf 'builduser ALL=(ALL) ALL\n' | tee -a /etc/sudoers
+
+cat ./gpg_key | base64 --decode | gpg --homedir /root/.gnupg --import
+cat ./gpg_key | base64 --decode | gpg --homedir /home/builduser/.gnupg --import
+rm ./gpg_key
+echo "checking out buildusers key"
+gpg --homedir /home/builduser/.gnupg --list-keys
+echo "checking out root key"
+gpg --homedir /root/.gnupg --list-keys
+
+sudo pacman -S base-devel --noconfirm --needed
+
+for i in "linux-volterra" ; do
+	status=13
+	git submodule update --init $i
+	cd $i
+
+	for i in $(sudo -u builduser makepkg --packagelist); do
+		package=$(basename $i)
+		wget https://github.com/$repo_owner/$repo_name/releases/download/packages/$package \
+			&& echo "Warning: $package already built, did you forget to bump the pkgver and/or pkgrel? It will not be rebuilt."
+	done
+	sudo -u builduser bash -c 'export MAKEFLAGS=-j$(nproc) && makepkg --sign -s --noconfirm'||status=$?
+
+	# Package already built is fine.
+	if [ $status != 13 ]; then
+		exit 1
+	fi
+	cd ..
+done
+
+cp */*.pkg.tar.* ./
+gpg --list-keys
+repo-add --sign ./$repo_owner-volterra.db.tar.gz ./*.pkg.tar.xz
+
+for i in *.db *.files; do
+cp --remove-destination $(readlink $i) $i
+done
